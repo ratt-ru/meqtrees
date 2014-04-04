142dfc3ba124af94daa03ab47a472fcb6083d598
Gijs Molenaar <gijs@pythonic.nl>
2014-04-04 16:34:31 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,7 @@
 #
 # $ curl -s https://raw.github.com/ska-sa/papino/master/ubuntu.sh | sudo sh
 #
-apt-get install -y python-software-properties
+apt-get install -y python-software-properties software-properties-common
 add-apt-repository ppa:ska-sa/main
 apt-get update
-apt-get install -y python-meqtrees-cattery python-meqtrees-timba casacore-data
+apt-get install -y meqtrees
